35fa6a95eeeb8d83675936b61748093b37f8a1dd
Erik Gimenez <Erikgs2006@gmail.com>
2025-07-02 22:04:33 -0300

    Delete AulaWeb/Exercicio1/exercicio1.js

--- AulaWeb/Exercicio1/exercicio1.js
+++
@@ -1,9 +0,0 @@
-function mudarTexto() {
-    document.getElementById("message").innerText = "Olá " + document.getElementById("nome").value;
-}
-
-function mudarEstilo() {
-    const p = document.getElementById("message");
-    p.style.color = "blue";
-    p.style.fontSize = "32px";
-}
